356e8225ce08139b61f185693ef57956d9773e8d
Rando <67262768+SomeNormalRando@users.noreply.github.com>
2021-09-21 13:18:58 +0800

    Update
    Some style and script changes, mainly storing the current font in storage and changing more elements' fonts

--- popup.js
+++ popup.js
@@ -3,6 +3,7 @@ const webSafeFonts = {
 	"sans-serif": [
 		"Arial",
 		"Arial Black",
+		"Arial Narrow",
 		"Verdana",
 		"Tahoma",
 		"Trebuchet MS",
@@ -10,24 +11,16 @@ const webSafeFonts = {
 	],
 	"serif": [
 		"Times New Roman",
-		"Didot",
 		"Georgia",
-		"American Typewriter"
 	],
 	"monospace": [
-		"Andalé Mono",
 		"Courier",
 		"Lucida Console",
-		"Monaco"
 	],
 	"cursive": [
-		"Bradley Hand",
 		"Brush Script MT",
 		"Comic Sans MS"
 	],
-	"fantasy": [
-		"Luminari"
-	]
 }
 
 //Get the select menu element
@@ -43,6 +36,18 @@ for (const fontCategory of Object.keys(webSafeFonts)) {
 	}
 }
 
+(async function() {
+	chrome.storage.sync.get("font", ({ font }) => {
+		document.getElementById("fontSelect").value = font;
+	});
+	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+	chrome.scripting.executeScript({
+		target: { tabId: tab.id },
+		function: changePageFont,
+	});
+})();
+
 // When an option is chosen, inject changeFont() into current page
 fontSelect.addEventListener("change", async (event) => {
 	chrome.storage.sync.set({ font: event.target.value });
@@ -57,10 +62,25 @@ fontSelect.addEventListener("change", async (event) => {
 // The body of this function will be executed as a content script inside the current page
 function changePageFont() {
 	chrome.storage.sync.get("font", ({ font }) => {
-		if (font === "default") {
-			document.body.style.fontFamily = "";
-		} else {
-			document.body.style.fontFamily = font;
+		const elements = [
+			document.getElementsByTagName("p"),
+			document.getElementsByTagName("em"),
+			document.getElementsByTagName("strong"),
+			document.getElementsByTagName("a"),
+			document.getElementsByTagName("div"),
+			document.getElementsByTagName("span"),
+		]
+
+		if (font === "default") font = "";
+		loop(font, elements);
+
+	});
+	function loop(font, elementsArr) {
+		document.body.style.fontFamily = font;
+		for (const elementType of elementsArr) {
+			for (const element of elementType) {
+				element.style.fontFamily = font;
+			}
 		}
-	})
+	}
 }
